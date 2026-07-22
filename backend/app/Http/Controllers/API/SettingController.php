89dea05555fbbb82ab8b2277c58345f28704b510
Pratik <wrickguha2003@gmail.com>
2026-07-23 00:25:00 +0530

    feat: add SettingController to retrieve application settings as JSON

--- backend/app/Http/Controllers/API/SettingController.php
+++ backend/app/Http/Controllers/API/SettingController.php
@@ -11,16 +11,7 @@ class SettingController extends Controller
 {
     public function index(Request $request): JsonResponse
     {
-        $group = $request->query('group');
-
-        $query = Setting::query();
-        if ($group) {
-            $query->where(function ($q) use ($group) {
-                $q->where('group', $group)
-                  ->orWhereNull('group');
-            });
-        }
-        $settings = $query->pluck('value', 'key');
+        $settings = Setting::pluck('value', 'key');
 
         return response()->json([
             'success' => true,
